2ceef991732e602adf687bfe0aed714f198cb094
qier <1323287928@qq.com>
2022-07-17 11:46:44 +0800

    完成了首页功能的开发

--- main.js
+++ main.js
@@ -2,9 +2,25 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
-
+import {$http} from "@escook/request-miniprogram"
 Vue.config.productionTip = false
-
+uni.$http=$http
+$http.beforeRequest=function(options){
+	uni.showLoading({
+		title:"数据加载中..."
+	})
+}
+$http.afterRequest=function(options){
+	uni.hideLoading()
+}
+$http.baseUrl="https://api-ugo-web.itheima.net"
+uni.$showMsg=function(title="数据请求失败！",duration=1500){
+	 uni.showToast({
+	 	title,
+		duration,
+		icon:"none"
+	 })
+}
 App.mpType = 'app'
 
 const app = new Vue({
